cfcd37fe91ddde044d593a6d7f9a9904e038bf1d
Angie Parra <98565270+catapaov@users.noreply.github.com>
2022-08-22 20:16:36 -0500

    Update logic.py

--- co2 calculator/logic.py
+++ co2 calculator/logic.py
@@ -2,6 +2,10 @@
  Logic 
  """
 
+"""
+Change the percentage of co2 emitted depending on the season
+ """
+
 def modified_rate_worst(initial_rate, season,seasons):
 
     rate= (initial_rate / (seasons[season][0]))
@@ -11,11 +15,15 @@ def modified_rate_best(initial_rate, season, seasons):
 
     rate= (initial_rate / (seasons[season][1]))
     return rate
+  
+"""
+Define the season according to the month, day and time of the event
+ """
 
 def set_season(month,day,time):
 
     season= "low"
-    months=[1]
+    months=["march","june","july","august","december","january"]
     days=["monday","friday","saturday","sunday"]
     times=["morning","night"]
 
@@ -27,7 +35,19 @@ def set_season(month,day,time):
         else:
             season= "medium"
     return season
-
+"""
+Calculate the total kg of CO2 that would lead to the event being
+carried out in person. Two scenarios are offered, one with 
+underestimates and the other with overestimates. For the calculation
+, the size of the event is taken into account, whether it is national,
+international, regional or local, how many people on average will 
+attend and the date on which the event will take place. The season 
+is used to determine how full the means of transport in which people
+move will be to estimate the amount of co2 per passenger, the type of
+event is used to estimate the distances that attendees would have to 
+travel and the possible means of transportation. transport that you
+would use because each one generates a different amount of co2
+ """
 
 def transport_calculator(people,continents,season, seasons,continent, type, types,transports):
 
@@ -64,6 +84,14 @@ def transport_calculator(people,continents,season, seasons,continent, type, type
 
 
     return total
+   
+  """
+Calculate the total kg of CO2 that would lead to the event being
+carried out in virtually. Take into account the duration and the
+places from where people would connect to the event, since in each 
+region the percentages of use of certain energy sources vary and 
+I get the amount of co2 emitted
+ """
 
 def virtual_calculator(people,continents,continent,duration,energy):
 
